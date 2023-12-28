325a13953df98f68506337afd0b1aebd02b60dec
ying1488 <yingecteach@gmail.com>
2023-12-28 14:14:25 +1100

    .

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,52 @@
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+
+interface SearchRepositoriesAction {
+  type: 'search_repositories';
+
+}
+interface SearchRepositoriesSuccessAction {
+  type: 'search_repositories_success';
+  payload: string[];
+
+}
+interface SearchRepositoriesErrorAction {
+  type: 'search_repositories_error';
+  payload: string;
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action:
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction
+): RepositoriesState => {
+  action
+
+  if (action.type === 'search_repositories_success') {
+    //100% certainty that 'action' satisfies the searchrepsuccessactions interface 
+    action.payload
+  }
+  switch (action.type) {
+    case 'search_repositories':
+      return { loading: true, error: null, data: [] };
+
+    case 'search_repositories_success':
+      //100% certain that 'action' is SearchRepositoriesSuccessAction
+      return { loading: false, error: null, data: action.payload };
+
+    case 'search_repositories_error':
+      return { loading: false, error: action.payload, data: [] };
+
+    default:
+      return state;
+  }
+};
+
+
+export default reducer;
